a1dd89bc79bb02221bd64baae786ed5d2edfddc2
Matteo Ridolfi <matteo.ridolfi@danielwellington.com>
2019-03-14 14:48:15 +0100

    stack-id stack-name

--- ecs/tag/create.go
+++ ecs/tag/create.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/dwtechnologies/custom-cf/lib/events"
 )
@@ -11,6 +13,19 @@ import (
 // settings specified by req.
 // Returns a map of properties and error.
 func (c *config) createTags(req *events.Request) (map[string]string, error) {
+	// append CF stack-id
+	c.resourceProperties.Tags = append(c.resourceProperties.Tags, ecs.Tag{
+		Key:   aws.String("cloudformation:stack-id"),
+		Value: aws.String(req.StackID),
+	})
+
+	// append CF stack-name
+	stackName := strings.Split(req.StackID, "/")[1]
+	c.resourceProperties.Tags = append(c.resourceProperties.Tags, ecs.Tag{
+		Key:   aws.String("cloudformation:stack-name"),
+		Value: aws.String(stackName),
+	})
+
 	_, err := c.svc.TagResourceRequest(
 		&ecs.TagResourceInput{
 			ResourceArn: &c.resourceProperties.ResourceARN,
