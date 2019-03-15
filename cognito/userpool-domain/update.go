265d8ccce372fc7503e7f3680503cd7bd36ae609
Anders Wallin <touch.dev.null@gmail.com>
2019-03-15 15:53:28 +0100

    Removed update due to buggy AWS API.

--- cognito/userpool-domain/update.go
+++ cognito/userpool-domain/update.go
@@ -1,44 +1,15 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 	"github.com/dwtechnologies/custom-cf/lib/events"
 )
 
 // updateDomain updates the domain specified in req.
 // Returns map[string]string and error.
 func (c *config) updateDomain(req *events.Request) (map[string]string, error) {
-	switch {
-	case c.resourceProperties.Domain == "":
-		return nil, fmt.Errorf("No Domain specified")
-
-	case c.resourceProperties.UserPoolID == "":
-		return nil, fmt.Errorf("No UserPoolId specified")
+	// Due to the API for UserPool Domain being so buggy we need to delete and create.
+	if err := c.deleteDomain(req); err != nil {
+		return nil, err
 	}
-
-	input := &cognitoidentityprovider.UpdateUserPoolDomainInput{
-		Domain:     &c.resourceProperties.Domain,
-		UserPoolId: &c.resourceProperties.UserPoolID,
-	}
-
-	// Only set CustomDomainConfig if it's not nil.
-	if c.resourceProperties.CustomDomainConfig != nil {
-		input.CustomDomainConfig = &cognitoidentityprovider.CustomDomainConfigType{
-			CertificateArn: &c.resourceProperties.CustomDomainConfig.CertificateArn,
-		}
-	}
-
-	resp, err := c.svc.UpdateUserPoolDomainRequest(input).Send()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to update Domain. Error %s", err.Error())
-	}
-
-	data := map[string]string{}
-	if resp.CloudFrontDomain != nil {
-		data["Domain"] = *resp.CloudFrontDomain
-	}
-
-	return data, nil
+	return c.createDomain(req)
 }
