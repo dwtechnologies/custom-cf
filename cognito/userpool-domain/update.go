1a8007395b39d04413d6a3f448936f1d437da7b8
Anders Wallin <touch.dev.null@gmail.com>
2019-03-15 15:35:55 +0100

    Removed update. The update part of the SDK doesn't even work... Replaced with delete then create.

--- cognito/userpool-domain/update.go
+++ cognito/userpool-domain/update.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 	"github.com/dwtechnologies/custom-cf/lib/events"
 )
 
@@ -18,27 +17,10 @@ func (c *config) updateDomain(req *events.Request) (map[string]string, error) {
 		return nil, fmt.Errorf("No UserPoolId specified")
 	}
 
-	input := &cognitoidentityprovider.UpdateUserPoolDomainInput{
-		Domain:     &c.resourceProperties.Domain,
-		UserPoolId: &c.resourceProperties.UserPoolID,
+	// The update part of the SDK doesn't work...
+	if err := c.deleteDomain(req); err != nil {
+		return nil, err
 	}
 
-	// Only set CustomDomainConfig if it's not nil.
-	if c.resourceProperties.CustomDomainConfig != nil {
-		input.CustomDomainConfig = &cognitoidentityprovider.CustomDomainConfigType{
-			CertificateArn: &c.resourceProperties.CustomDomainConfig.CertificateArn,
-		}
-	}
-
-	resp, err := c.svc.UpdateUserPoolDomainRequest(input).Send()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to update Domain. Error %s", err.Error())
-	}
-
-	data := map[string]string{}
-	if resp.CloudFrontDomain != nil {
-		data["Domain"] = *resp.CloudFrontDomain
-	}
-
-	return data, nil
+	return c.createDomain(req)
 }
